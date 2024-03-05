00ce94a75b4481acdeb48ad8dbec9732f246ce2e
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-05 11:22:29 +0000

    resource lock

--- backup-sa.tf
+++ backup-sa.tf
@@ -13,25 +13,21 @@ resource "azurerm_storage_account" "backup" {
   account_kind = "StorageV2"
 
   lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [tags]
+    ignore_changes = [tags]
   }
 
   network_rules {
-    default_action             = "Deny"
-    ip_rules                   = var.org_ip_addresses
-    virtual_network_subnet_ids = concat([var.subnet_ids], var.build_agent_subnet_ids)
-  bypass                       = ["AzureServices"]
+  default_action             = "Deny"
+  ip_rules                   = var.org_ip_addresses
+  virtual_network_subnet_ids = concat([var.subnet_ids], var.build_agent_subnet_ids)
+  bypass                     = ["AzureServices"]
   }
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "data-lake-backup" {
   name               = "adls-backup"
   storage_account_id = azurerm_storage_account.backup.id
-  depends_on = [ azurerm_storage_account.backup ]
-  lifecycle {
-    prevent_destroy = true
-  }
+  depends_on         = [ azurerm_storage_account.backup ]
 }
 
 resource "azurerm_key_vault_secret" "backup_storage_key" {
@@ -43,4 +39,12 @@ resource "azurerm_key_vault_secret" "backup_storage_key" {
   lifecycle {
     ignore_changes = [tags]
   }
+}
+
+resource "azurerm_management_lock" "adls_lock" {
+  name       = "adls-storageaccount-lock"
+  scope      = azurerm_storage_account.backup.id
+  lock_level = "CanNotDelete"
+  notes      = "To protect Prod Data"
+  count      = (var.environment_name == "prod" || var.environment_name == "live") ? 1 : 0
 }
