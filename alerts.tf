e5933d7073972755cedc1d0f70460806b267ca3c
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-19 13:24:45 +0000

    setup alerting

---
+++ alerts.tf
@@ -0,0 +1,19 @@
+resource "azurerm_monitor_metric_alert" "adf_pipeline_failure_alert" {
+  name                = "adf-pipeline-failure-alert"
+  resource_group_name = var.resource_group_name
+  scopes              = [ azurerm_data_factory.this.id ]
+
+criteria {
+    metric_namespace = "Microsoft.DataFactory/factories/pipelines"
+    metric_name      = "FailedRuns"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+}
+
+## You will need this created in your terraform
+
+action {
+    action_group_id = var.monitor_action_group
+    }
+}
--- variables.tf
+++ variables.tf
@@ -56,6 +56,9 @@ variable "main_storage_account_primary_dfs_endpoint" {
 variable "source_container" {
   description = "source container that the data is being backed up from"
 }
+variable "monitor_action_group" {
+  description = "monitor action group reference"
+}
 
 
 
