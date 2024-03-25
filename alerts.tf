3bfe6b356b07d721383a5a42df9a03069575ffce
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-25 14:41:12 +0000

    action group in module

--- alerts.tf
+++ alerts.tf
@@ -1,3 +1,15 @@
+resource "azurerm_monitor_action_group" "adf_alerts_monitor" {
+    name = "${var.org}-${var.name}-adf-alerts-monitor-${var.environment_name}"
+    resource_group_name = var.resource_group_name
+    short_name = "${var.name}-adf-alerts-${var.environment_name}"
+
+    email_receiver {
+    name = "Email"
+    email_address = var.alerts_email
+    use_common_alert_schema = true
+    }
+}
+
 resource "azurerm_monitor_metric_alert" "adf_pipeline_failure_alert" {
     name                = "adf-pipeline-failure-alert"
     provider            = azurerm.sub
@@ -15,6 +27,6 @@ criteria {
 ## You will need the action group created in your terraform
 
 action {
-    action_group_id = var.monitor_action_group_id
+    action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
     }
 }
--- variables.tf
+++ variables.tf
@@ -56,9 +56,8 @@ variable "main_storage_account_primary_dfs_endpoint" {
 variable "source_container" {
   description = "source container that the data is being backed up from"
 }
-variable "monitor_action_group_id" {
-  description = "monitor action group reference id"
+variable "alerts_email" {
+description = "email for alerts"
 }
-
 
 
