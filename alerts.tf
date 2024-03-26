4e1ace8fc77087623fcecc2c3e6a7582ca2cc02e
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-26 11:15:10 +0000

    testing success

--- alerts.tf
+++ alerts.tf
@@ -28,3 +28,22 @@ action {
     action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
     }
 }
+
+resource "azurerm_monitor_metric_alert" "adf_pipeline_success_alert" {
+    name                = "adf-pipeline-success-alert"
+    provider            = azurerm.sub
+    resource_group_name = var.resource_group_name
+    scopes              = [ azurerm_data_factory.this.id ]
+
+criteria {
+    metric_namespace = "Microsoft.DataFactory/factories"
+    metric_name      = "PipelineSucceededRuns"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+}
+
+action {
+    action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
+    }
+}
