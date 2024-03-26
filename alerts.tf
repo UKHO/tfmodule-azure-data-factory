315348906cb4961da368f51f33c12e48992a4a25
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-26 10:32:24 +0000

    short name shorted

--- alerts.tf
+++ alerts.tf
@@ -1,7 +1,7 @@
 resource "azurerm_monitor_action_group" "adf_alerts_monitor" {
     name                = "${var.org}-${var.name}-adf-alerts-monitor-${var.environment_name}"
     resource_group_name = var.resource_group_name
-    short_name          = "${var.name}-adf-alerts-${var.environment_name}"
+    short_name          = "${var.name}alerts${var.environment_name}"
     provider            = azurerm.sub
 
     webhook_receiver {
