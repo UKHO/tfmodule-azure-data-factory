f6fd4c5cc9b9c557ea1b72b27785d9bbd92e649c
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-26 14:29:52 +0000

    ignore tags

--- alerts.tf
+++ alerts.tf
@@ -4,6 +4,10 @@ resource "azurerm_monitor_action_group" "adf_alerts_monitor" {
     short_name          = "${var.name}alerts${var.environment_name}"
     provider            = azurerm.sub
 
+    lifecycle {
+        ignore_changes = [tags]
+    }
+
     webhook_receiver {
         name        = "TeamsWebhook"
         service_uri = var.teams_url
@@ -16,16 +20,20 @@ resource "azurerm_monitor_metric_alert" "adf_pipeline_failure_alert" {
     resource_group_name = var.resource_group_name
     scopes              = [ azurerm_data_factory.this.id ]
 
-criteria {
-    metric_namespace = "Microsoft.DataFactory/factories"
-    metric_name      = "PipelineFailedRuns"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 0
+    criteria {
+        metric_namespace = "Microsoft.DataFactory/factories"
+        metric_name      = "PipelineFailedRuns"
+        aggregation      = "Total"
+        operator         = "GreaterThan"
+        threshold        = 0
 }
 
-action {
-    action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
+    action {
+        action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
+    }
+
+    lifecycle {
+        ignore_changes = [tags]
     }
 }
 
@@ -35,15 +43,19 @@ resource "azurerm_monitor_metric_alert" "adf_pipeline_success_alert" {
     resource_group_name = var.resource_group_name
     scopes              = [ azurerm_data_factory.this.id ]
 
-criteria {
-    metric_namespace = "Microsoft.DataFactory/factories"
-    metric_name      = "PipelineSucceededRuns"
-    aggregation      = "Total"
-    operator         = "GreaterThan"
-    threshold        = 0
+    criteria {
+        metric_namespace = "Microsoft.DataFactory/factories"
+        metric_name      = "PipelineSucceededRuns"
+        aggregation      = "Total"
+        operator         = "GreaterThan"
+        threshold        = 0
 }
 
 action {
-    action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
+        action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
+    }
+
+    lifecycle {
+        ignore_changes = [tags]
     }
 }
