03a67a64564b1bb0dc521ee618838fbed1b46f8e
Sam Titmuss <sam.titmuss@ukho.gov.uk>
2024-03-27 08:35:55 +0000

    testing with successful

--- alerts.tf
+++ alerts.tf
@@ -41,3 +41,31 @@ resource "azurerm_monitor_metric_alert" "adf_pipeline_failure_alert" {
         ignore_changes = [tags]
     }
 }
+
+resource "azurerm_monitor_metric_alert" "adf_pipeline_success_alert" {
+    name                = "adf-pipeline-success-alert"
+    provider            = azurerm.sub
+    resource_group_name = var.resource_group_name
+    scopes              = [ azurerm_data_factory.this.id ]
+
+    criteria {
+        metric_namespace = "Microsoft.DataFactory/factories"
+        metric_name      = "PipelineSucceededRuns"
+        aggregation      = "Total"
+        operator         = "GreaterThan"
+        threshold        = 0
+        dimension {
+            name     = "PipelineName"
+            operator = "Include"
+            values   = ["${var.environment_name}DataLakeBackupFull", "${var.environment_name}DataLakeBackupIncremental"]
+        }
+}
+
+action {
+        action_group_id = azurerm_monitor_action_group.adf_alerts_monitor.id
+    }
+
+    lifecycle {
+        ignore_changes = [tags]
+    }
+}
