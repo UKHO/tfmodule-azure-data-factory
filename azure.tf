68f8077bf2e376a5a4a21b85906c379246ced94c
Kieran O'Hanlon <akieran@ukho.gov.uk>
2026-02-13 15:39:45 +0000

    Removed versions.tf and updtaed to just use azure.tf

--- azure.tf
+++ azure.tf
@@ -1,11 +1,26 @@
 terraform {
+  required_version = ">= 1.2.0"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = ">= 3.54"
       configuration_aliases = [
         azurerm.sub,
         azurerm.hub
       ]
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9"
     }
   }
 }
